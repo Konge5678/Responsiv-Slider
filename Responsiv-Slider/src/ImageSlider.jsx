a89f16b39c4d20573fb961c6de694549bc744821
KrisH <123541182+Konge5678@users.noreply.github.com>
2024-05-09 22:37:02 +0200

    More responsive and added a slider counter which shows which slide ur on

--- Responsiv-Slider/src/ImageSlider.jsx
+++ Responsiv-Slider/src/ImageSlider.jsx
@@ -12,38 +12,72 @@ function ImageSlider() {
 
   const [content, setContent] = useState(null);
 
-  useEffect(() => {
-    if (slides[currentSlide].type === 'api') {
-      slides[currentSlide].content().then(data => setContent(data));
+  const updateContent = (slideIndex) => {
+    if (slides[slideIndex].type === 'api') {
+        slides[slideIndex].content().then(data => setContent(data));
     } else {
-      setContent(slides[currentSlide].content);
+        setContent(slides[slideIndex].content);
     }
+  }
+
+  useEffect(() => {
+    updateContent(currentSlide);
   }, [currentSlide]);
 
   const nextSlide = () => {
-    setCurrentSlide((currentSlide + 1) % slides.length);
+    setCurrentSlide(prevSlide => {
+        const newSlideIndex = (prevSlide + 1) % slides.length;
+        updateContent(newSlideIndex);
+        return newSlideIndex;
+    });
   };
 
   const prevSlide = () => {
-    setCurrentSlide((currentSlide - 1 + slides.length) % slides.length);
+    setCurrentSlide(prevSlide => {
+        const newSlideIndex = (prevSlide - 1 + slides.length) % slides.length;
+        updateContent(newSlideIndex);
+        return newSlideIndex;
+    });
   };
 
   return (
     <div className="flex flex-col sm:flex-row items-center justify-center bg-gray-200 p-5 space-x-0 sm:space-x-5">
       <div className="hidden sm:flex justify-between items-center w-full">
         <button className="py-2 px-4 text-white bg-blue-500 rounded-md" onClick={prevSlide}>Previous</button>
-        <div className="w-full sm:w-96 h-64 sm:h-96 rounded-md bg-blue-600">
-          {slides[currentSlide].type === 'image' && <img src={content} alt="" className="w-full h-full" />}
-          {slides[currentSlide].type === 'api' && content}
-          {slides[currentSlide].type === 'video' && <video src={content} className="w-full h-full" autoPlay loop />}
+        <div className="w-full sm:w-96 h-64 sm:h-96 rounded-md bg-blue-600 relative">
+          <div className="w-full h-full absolute">
+            {slides[currentSlide].type === 'image' && <img src={content} alt="" className="w-full h-full object-cover" />}
+          </div>
+          <div className="absolute">
+            {slides[currentSlide].type === 'api' && content}
+          </div>
+          <div className="absolute">
+            {slides[currentSlide].type === 'video' && <video src={content} className="w-full h-full object-contain" autoPlay loop />}
+          </div>
+          <div className="absolute bottom-0 left-0 right-0 flex justify-center space-x-2 p-2">
+            {slides.map((slide, index) => (
+              <span key={index} className={`h-1 w-1 rounded-full ${currentSlide === index ? 'bg-white' : 'bg-gray-500'}`}></span>
+            ))}
+          </div>
         </div>
         <button className="py-2 px-4 text-white bg-blue-500 rounded-md" onClick={nextSlide}>Next</button>
       </div>
       <div className="sm:hidden flex flex-col items-center w-full mt-5">
-        <div className="w-full h-64 rounded-md bg-blue-600">
-          {slides[currentSlide].type === 'image' && <img src={content} alt="" className="w-full h-full" />}
-          {slides[currentSlide].type === 'api' && content}
-          {slides[currentSlide].type === 'video' && <video src={content} className="w-full h-full" autoPlay loop />}
+        <div className="w-full h-64 rounded-md bg-blue-600 relative">
+          <div className="w-full h-full absolute">
+            {slides[currentSlide].type === 'image' && <img src={content} alt="" className="w-full h-full object-cover" />}
+          </div>
+          <div className="absolute">
+            {slides[currentSlide].type === 'api' && content}
+          </div>
+          <div className="absolute">
+            {slides[currentSlide].type === 'video' && <video src={content} className="w-full h-full object-contain" autoPlay loop />}
+          </div>
+          <div className="absolute bottom-0 left-0 right-0 flex justify-center space-x-2 p-2">
+            {slides.map((slide, index) => (
+              <span key={index} className={`h-1 w-1 rounded-full ${currentSlide === index ? 'bg-white' : 'bg-gray-500'}`}></span>
+            ))}
+          </div>
         </div>
         <div className="flex justify-around w-full mt-5">
           <button className="py-2 px-4 text-white bg-blue-500 rounded-md" onClick={prevSlide}>Previous</button>
